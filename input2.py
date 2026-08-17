9d4a00c200b62b17f844dc748e8a2f6613d0c677
Mohammed Ismail <Mohammed-Ismail543>
2026-08-17 21:23:56 +0530

    add file successfully

---
+++ input2.py
@@ -0,0 +1,7 @@
+item = input("what would you like to buy ")
+Price = float(input("what is the price:"))
+quantity = int(input("how many items would you like"))
+total = Price * quantity
+
+print(f"you have bought {quantity} x {item}/s")
+print(f"your total is : {total}")
